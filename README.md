62be881e18baf5b6cfac99739ee8432d71e92d2f
jeffrey.maier <money11maier@gmail.com>
2022-12-01 15:32:42 -0700

    refactored. simplified and added md description.

--- README.md
+++ README.md
@@ -4,3 +4,18 @@
 
 I am using nflfastR to gather the nfl data.
 
+### Expected Fantasy Points
+
+This program is written to gather a single year or multiple year's worth of play by play data.
+
+Here is what it then does:
+
+    1. Adds a fantasy points column
+    2. Creates two tables. 1 for rushing and 1 for receiving expected fantasy points. 
+    3. Iterate over each row, check if it's a rush or pass attempt and if it is add in expected points.
+    4. Group by Player then total fantasy points, expected points, add in a column for fantasy points over expected and sort by total fantasy points.
+    5. Writes to csv as "players_grouped.csv" in the current folder.
+
+### Uses
+
+This is descriptive stat and not predictive. So it's best use is likely in the offseason when multiple players may be ranked similarly you may use the fantasy points over expected to differentiate between them. I could also see this used in season to evaluate players that may seem 'hot' or 'cold'. If someone is being used and expected to score 15-20 points a game but they are actually scoring 10 it might be worth to stick with them and on the other side if someone is expected to score 10 but actually scoring 20 it may be beneficial to sell them off. 
--- expected_fantasy_points.r
+++ expected_fantasy_points.r
@@ -19,32 +19,27 @@ seasons <- seasons %>% mutate (
 rush_fp_yardline <- seasons %>% filter(rush_attempt == 1 & qb_kneel == 0) %>% group_by(yardline_100) %>% summarise(rush_fp_avg = mean(fp, na.rm = T), plays = n())
 receiving_fp_yardline <- seasons %>% filter(pass_attempt == 1 & qb_spike == 0 & sack == 0) %>% group_by(yardline_100) %>% summarise(rec_fp_avg = mean(fp, na.rm = T), plays = n())
 
-# Let's separate rushing plays and receiving plays to make this easier to go over 
-rushing_plays <- seasons %>% filter(rush_attempt == 1 &  qb_kneel == 0)
-receiving_plays <- seasons %>% filter(pass_attempt == 1 & qb_spike == 0) 
 
 # getting ready to add in the advanced expected fantasy points to our rushing and receiving df's we add the column for it before looping over each to add
-rushing_plays$advanced_fp <- seq(1, nrow(rushing_plays))
-receiving_plays$advanced_fp <- seq(1, nrow(receiving_plays))
+seasons$advanced_fp <- seq(1, nrow(seasons))
 
-# here we should just write a function but I'm a little lazy cause I'm only doing this twice
-for (i in 1:nrow(rushing_plays)) {
-   yardline <- rushing_plays$yardline_100[i]
-   rushing_plays$advanced_fp[i] <- unlist(rush_fp_yardline[yardline, 2])
+# iterate over each row, check if it's a rush or pass attempt and if it is add in expected points. 
+for (i in 1:nrow(seasons)) {
+  yardline <- seasons$yardline_100[i]
+  if (is.na(seasons$rush_attempt[i]) | is.na(seasons$pass_attempt[i])) {
+    next
+  } else if (seasons$rush_attempt[i] == 1) {
+    seasons$advanced_fp[i] <- unlist(rush_fp_yardline[yardline, 2])
+    print(i)
+  } else if (seasons$pass_attempt[i] == 1) {
+    seasons$advanced_fp[i] <- unlist(receiving_fp_yardline[yardline, 2])
+    print(i)
+  }
 }
-
-for (i in 1:nrow(receiving_plays)) {
-   yardline <- receiving_plays$yardline_100[i]
-   receiving_plays$advanced_fp[i] <- unlist(receiving_fp_yardline[yardline, 2])
-}
-
-
-# let's combine these two dataframes
-rushing_and_receiving <- rbind(rushing_plays, receiving_plays)
 
 # Here we group by player to see their totals. We then add fp over expected and then write the csv to whatever folder you run this from.
-players_grouped <- rushing_and_receiving %>% group_by(fantasy_player_id, fantasy_player_name) %>% summarise(total_fp = sum(fp, na.rm = T), total_adv_fp = sum(advanced_fp, na.rm = T), plays = n()) %>% arrange(-total_fp)
-players_grouped$fp_over_exp <- players_grouped$total_fp - players_grouped$total_adv_fp
+players_grouped <- seasons %>% group_by(fantasy_player_id, fantasy_player_name) %>% summarise(total_fp = sum(fp, na.rm = T), total_adv_fp = sum(advanced_fp, na.rm = T), plays = n()) %>% mutate(fp_over_exp = total_fp - total_adv_fp) %>% arrange(-total_fp)
+
 write.csv(players_grouped, "players_grouped.csv")
 
 print("done")
