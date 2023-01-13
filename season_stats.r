36177bb9d72d04553324f6cf423f87a0a7ae915b
Jeffrey Maier <118404179+jeffreymaier@users.noreply.github.com>
2023-01-13 09:13:45 -0700

    Update season_stats.r
    updating to include team totals

--- season_stats.r
+++ season_stats.r
@@ -1,15 +1,62 @@
 library(nflfastR)
 library(tidyverse)
 
-season <- nflfastR::load_player_stats(2022)
+season <- nflfastR::load_player_stats(1999:2022)
 
 offensive_players <- season %>% filter(position_group == "QB" | position_group == "RB" | position_group == "WR" | position_group == "TE")
 
-qb_totals <- offensive_players %>% 
-  filter(position_group == "QB") %>% 
-  group_by(player_id, player_display_name, position_group, recent_team) %>% 
+totals <- offensive_players %>% 
+  filter(season_type == "REG") %>% 
+  group_by(season, player_id, player_display_name, position_group) %>% 
   summarise(games = n(),
             att = sum(attempts, na.rm = T),
+            comp = sum(completions, na.rm = T),
+            comp_percent = comp / att,
+            pass_yards = sum(passing_yards, na.rm = T),
+            ypa = pass_yards / att,
+            pass_td = sum(passing_tds, na.rm = T),
+            td_att = pass_td / att,
+            int = sum(interceptions, na.rm = T),
+            int_att = int / att,
+            sack = sum(sacks, na.rm = T),
+            spg = sack / games,
+            sack_yard = sum(sack_yards, na.rm = T),
+            yps = sack_yard / sack,
+            sack_fum = sum(sack_fumbles, na.rm = T),
+            sack_fum_lost = sum(sack_fumbles_lost, na.rm = T),
+            pass_air_yards = sum(passing_air_yards, na.rm = T),
+            aypa = pass_air_yards / att,
+            ay_conversion = pass_yards / pass_air_yards,
+            pass_yac = sum(passing_yards_after_catch, na.rm = T),
+            pyacpa = pass_yac / att,
+            carries = sum(carries, na.rm = T),
+            rush_yards = sum(rushing_yards, na.rm = T),
+            ypc = rush_yards / carries,
+            tds = sum(rushing_tds, na.rm = T),
+            fumbles = sum(rushing_fumbles, na.rm = T),
+            fumbles_lost = sum(rushing_fumbles_lost, na.rm = T),
+            first_down = sum(rushing_first_downs, na.rm = T),
+            tgts = sum(targets, na.rm = T),
+            catches = sum(receptions, na.rm = T),
+            cpct = catches / tgts,
+            rec_yards = sum(receiving_yards, na.rm = T),
+            rypt = rec_yards / tgts,
+            rec_air_yards = sum(receiving_air_yards, na.rm = T),
+            raypt = rec_air_yards / tgts,
+            ryac = sum(receiving_yards_after_catch, na.rm = T),
+            ryacpt = ryac / tgts,
+            tgt_share = mean(target_share, na.rm = T),
+            air_share = mean(air_yards_share, na.rm = T),
+            total_opps = carries + tgts,
+            total_yards = rush_yards + rec_yards,
+            fp = sum(fantasy_points_ppr, na.rm = T),
+            fppg = fp / games,
+  )
+
+qualifying_qb <- qb_totals %>% filter(att >= 150)
+
+teams <- season %>% filter(season_type == "REG") %>% group_by(recent_team, season) %>% 
+  summarise(att = sum(attempts, na.rm = T),
             comp = sum(completions, na.rm = T),
             comp_percent = comp / att,
             pass_yards = sum(passing_yards, na.rm = T),
@@ -28,14 +75,22 @@ qb_totals <- offensive_players %>%
             ay_conversion = pass_yards / pass_air_yards,
             pass_yac = sum(passing_yards_after_catch, na.rm = T),
             pyacpa = pass_yac / att,
-            rush_att = sum(carries, na.rm = T),
-            rush_yards =  sum(rushing_yards, na.rm = T),
-            rush_td = sum(rushing_tds, na.rm = T),
-            rush_ypa = rush_yards / rush_att,
-            rush_1d = sum(rushing_first_downs, na.rm = T),
-            rush_1dpa = rush_1d / rush_att,
-            fp = sum(fantasy_points_ppr, na.rm = T),
-            fppg = fp / games,
-  )
-
-qualifying_qb <- qb_totals %>% filter(att >= 150)
+            carries = sum(carries, na.rm = T),
+            rush_yards = sum(rushing_yards, na.rm = T),
+            ypc = rush_yards / carries,
+            tds = sum(rushing_tds, na.rm = T),
+            fumbles = sum(rushing_fumbles, na.rm = T),
+            fumbles_lost = sum(rushing_fumbles_lost, na.rm = T),
+            first_down = sum(rushing_first_downs, na.rm = T),
+            tgts = sum(targets, na.rm = T),
+            catches = sum(receptions, na.rm = T),
+            cpct = catches / tgts,
+            rec_yards = sum(receiving_yards, na.rm = T),
+            rypt = rec_yards / tgts,
+            rec_air_yards = sum(receiving_air_yards, na.rm = T),
+            raypt = rec_air_yards / tgts,
+            ryac = sum(receiving_yards_after_catch, na.rm = T),
+            ryacpt = ryac / tgts,
+            total_opps = carries + tgts,
+            total_yards = rush_yards + rec_yards,
+            )
